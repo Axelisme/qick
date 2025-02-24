19049593b046dcb510c2a5e23ed9b21f38b59362
Axelisme <jary33333@gmail.com>
2025-02-24 13:38:53 +0800

    remove dry run

--- __init__.py
+++ __init__.py
@@ -1,8 +1,6 @@
 import os
 import platform
 
-DRY_RUN = False
-
 
 def get_version():
     """Read library version from qick_lib/qick/VERSION (a text file containing only the version number).
@@ -69,51 +67,3 @@ if (
         from .qick import QickSoc
     except Exception as e:
         print("Could not import QickSoc:", e)
-
-if DRY_RUN:
-    import numpy as np
-    import Pyro4
-
-    # if we're not on a Zynq, we need to mock the QickSoc class
-    SocIp = list
-
-    class QickSoc:
-        def __init__(self):
-            self.autoproxy = []
-
-        @Pyro4.expose
-        def get_cfg(self):
-            return {}
-
-        def adcfreq(self, fpt, *args, **kwargs):
-            return fpt
-
-        def us2cycles(self, us, *args, **kwargs):
-            if isinstance(us, np.ndarray):
-                return (us * 1e3).astype(int)
-            return int(us * 1e3)
-
-        def cycles2us(self, cycles, *args, **kwargs):
-            if isinstance(cycles, np.ndarray):
-                return cycles.astype(float) / 1e3
-            return float(cycles) / 1e3
-
-    class QickConfig:
-        def __init__(self, cfg=None):
-            pass
-
-        def __repr__(self):
-            return "Dummy QickConfig"
-
-        def adcfreq(self, fpt, *args, **kwargs):
-            return fpt
-
-        def us2cycles(self, us, *args, **kwargs):
-            if isinstance(us, np.ndarray):
-                return (us * 1e3).astype(int)
-            return int(us * 1e3)
-
-        def cycles2us(self, cycles, *args, **kwargs):
-            if isinstance(cycles, np.ndarray):
-                return cycles.astype(float) / 1e3
-            return float(cycles) / 1e3
